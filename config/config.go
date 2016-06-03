01200ef4cf1a133a1950cc8f34394bdabd746c5e
Rob Kiefer <rob.kiefer@gmail.com>
2016-06-03 14:59:53 -0400

    release: v0.7.5

--- config/config.go
+++ config/config.go
@@ -15,7 +15,7 @@ type iobeamConfig struct {
 
 const (
 	// CLIVersion is the version of the CLI.
-	CLIVersion = "0.7.4"
+	CLIVersion = "0.7.5"
 	// DefaultApiServer is the default iobeam server.
 	DefaultApiServer = "https://api.iobeam.com"
 
